94eaecc711b9e81604ead87a7011fd1fa872cb56
Dens777 <dens77712@gmail.com>
2024-06-02 13:13:17 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name="Denis"
+print("Name:",name)
+age=44
+print("Age:",age)
+new_age=44+1
+print("New age:",new_age,"(Future congratulations!)")
+is_student=bool(age<new_age)
+print("Is Student:",is_student)
